76b27c587628a0fd5a78d6aadc8ded55a04e6785
Travis Craft <tlcraft@users.noreply.github.com>
2018-10-30 22:49:20 -0600

    Adding array of mock heroes

---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Superman' },
+    { id: 12, name: 'Wonder Woman' },
+    { id: 13, name: 'Narco' },
+    { id: 14, name: 'Glass' },
+    { id: 15, name: 'Mr. Nice' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
