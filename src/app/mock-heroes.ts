264afb7c53cf385c2fe3d88aacdc99eabac2f20e
Travis Craft <tlcraft@users.noreply.github.com>
2018-11-01 16:47:01 -0600

    Updating names to single quotes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,9 +1,9 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-    { id: 11, name: "Mr. Nice" },
-    { id: 12, name: "Windstorm" },
-    { id: 13, name: "Narco" },
-    { id: 14, name: "Bombasto" },
-    { id: 15, name: "Celeritas" }
-]
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Windstorm' },
+    { id: 13, name: 'Narco' },
+    { id: 14, name: 'Bombasto' },
+    { id: 15, name: 'Celeritas' }
+];
