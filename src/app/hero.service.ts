402fbdfdabb84f06a1aac4686495f2fcf68a16c2
Travis Craft <tlcraft@users.noreply.github.com>
2018-11-04 20:24:08 -0700

    Adding initial Dashboard page and routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,7 +11,7 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
-  getHeros(): Observable<Hero[]> {
+  getHeroes(): Observable<Hero[]> {
     //TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
