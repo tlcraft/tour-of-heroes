dd4be679cabc8abb0b941479bf1867eaf6755735
Travis Craft <tlcraft@users.noreply.github.com>
2018-11-05 20:18:49 -0700

    Complete the routing tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,7 +9,6 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
   
@@ -20,9 +19,5 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 }
