bb69ad1b1f61a3519243acf744190119a0ac00ed
Travis Craft <tlcraft@users.noreply.github.com>
2018-11-11 12:38:40 -0700

    Updating the memory data service

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,6 +1,10 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Hero } from './hero';
+import { Injectable } from '@angular/core';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
