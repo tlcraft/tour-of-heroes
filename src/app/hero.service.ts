cd47121d9f1641b55124fa4bf00e16a7169bbdbd
Travis Craft <tlcraft@users.noreply.github.com>
2018-11-21 18:31:25 -0700

    Add searchHeroes endpoint

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -58,6 +58,19 @@ export class HeroService {
     );
   }
 
+  /** GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if(!term.trim()) {
+      // if there is no search term, return an empty hero array
+      return of([]);
+    } 
+    
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
       tap(_ => this.log(`update hero id=$(hero.id}`)),
