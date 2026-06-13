ecdf9930877fcc36678e1d137951a37f3094f5b4
Travis Craft <tlcraft@users.noreply.github.com>
2026-06-13 12:06:52 -0600

    Add search logic to favorite hero component.

--- src/app/favorite-hero/favorite-hero.component.ts
+++ src/app/favorite-hero/favorite-hero.component.ts
@@ -1,13 +1,38 @@
 import { Component, inject } from '@angular/core';
 import { FavoriteHeroService } from '../favorite-hero.service';
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-favorite-hero',
+  imports: [AsyncPipe],
   templateUrl: './favorite-hero.component.html',
   styleUrls: ['./favorite-hero.component.scss'],
 })
 export class FavoriteHeroComponent {
   favoriteHeroService = inject(FavoriteHeroService);
+  heroes$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+  private heroService = inject(HeroService);
+  
+  constructor() {
+    this.heroes$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+    );
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 
   getFavoriteHero() {
     return this.favoriteHeroService.getFavoriteHeroName();
